dfd52cff7d51557dfc32ead857062145336c8f20
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-01 15:12:28 +0900

    update: userコントローラー作成？

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,4 @@ Rails.application.routes.draw do
   mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   root  "blogs#index"
   resources :blogs
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
