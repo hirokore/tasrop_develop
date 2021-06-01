515e505677207120ceda7c58cbe14aeac504e89d
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-01 10:56:33 +0900

    update: Devise用のviewsファイル準備

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   root  "blogs#index"
   resources :blogs
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
