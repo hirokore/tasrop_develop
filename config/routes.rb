6259a5975d619b02b51a7f4f2192b32fd3ef28e8
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-02 16:52:09 +0900

    update: routesの記載順は重要（作成したusersは下）

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
   root  "blogs#index"
-  resources :users, only: [:show, :index]
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
+  resources :users, only: [:show, :index]
   mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   resources :blogs
 end
