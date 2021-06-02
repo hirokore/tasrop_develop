51a012a34ec3028c75b7272f491bd2b91c9dda86
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-02 16:03:03 +0900

    update: devise利用時のユーザー一覧と詳細表示OK

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root  "blogs#index"
+  resources :users, only: [:show, :index]
   devise_for :users
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
