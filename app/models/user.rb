5bd7a8fb7aa0b230f4166e2d4fd2c7d8e7be516f
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-02 15:27:13 +0900

    update: ゲストログイン機能追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable,:confirmable
+         :recoverable, :rememberable, :validatable
 
   enum role: [:user, :vip, :admin]
   after_initialize :set_default_role, :if => :new_record?
@@ -11,4 +11,10 @@ class User < ApplicationRecord
     self.role ||= :user
   end
   
+  def self.guest
+    find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.vip!
+    end
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   root  "blogs#index"
+  devise_for :users
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
+  mount LetterOpenerWeb::Engine, at: "/mail" if Rails.env.development?
   resources :blogs
 end
