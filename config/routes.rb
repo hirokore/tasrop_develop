c46ab0f430e87b34764b3c929c7f30fd663833c6
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-03 16:09:37 +0900

    update: omniauth(google)を追加5(ラスト)

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'oauth_test/index'
   root  "blogs#index"
   devise_for :users, controllers: {
-    registrations: 'users/registrations'
+    registrations: 'users/registrations',
+    omniauth_callbacks: "users/omniauth_callbacks"
   }
+  get 'oauth_test/index'  
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
   end
