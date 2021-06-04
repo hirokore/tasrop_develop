fe9941a606d0b78db7d375d249e542552b3d81c2
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-05 01:15:05 +0900

    update: 繋がり機能4(コントローラ追加)

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'relationships/create'
+  get 'relationships/destroy'
   root  "blogs#index"
   devise_for :users, controllers: {
     registrations: 'users/registrations',
