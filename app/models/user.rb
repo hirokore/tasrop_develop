4a3a7d8653852e48e9ddf15bcf6adfa13dba858f
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-05 17:55:38 +0900

    update: 繋がり機能5(jquery追加、Ajax設定、他メソッド作成)

--- app/models/user.rb
+++ app/models/user.rb
@@ -39,4 +39,16 @@ class User < ApplicationRecord
     user.save
     user
   end
+  #指定のユーザをフォローする
+  def follow!(other_user)
+    active_relationships.create!(followed_id: other_user.id)
+  end
+  #フォローしているかどうかを確認する
+  def following?(other_user)
+    active_relationships.find_by(followed_id: other_user.id)
+  end
+  #指定のユーザのフォローを解除する
+  def unfollow!(other_user)
+    active_relationships.find_by(followed_id: other_user.id).destroy
+  end
 end
