f9c28641fd505288a4706de9af875eb8342546f0
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-10 18:11:57 +0900

    update: サイドバー作成、deviseのログイン時のパスを変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,7 @@ class ApplicationController < ActionController::Base
       redirect_to  '/users/sign_in',notice: "ログインしてください。"
     end
   end
+  def after_sign_in_path_for(resource)
+    current_user
+  end
 end
