571dba31ae4be9074ed0210d9cf7d1d994af5adb
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-14 18:28:53 +0900

    update: custom一覧ページとモデル設定漏れ 修正（custom）

--- app/controllers/customs_controller.rb
+++ app/controllers/customs_controller.rb
@@ -1,5 +1,14 @@
 class CustomsController < ApplicationController
+  def new
+    @custom = Custom.new
+  end
+  
+  def create
+    @custom = current_user.custom.new(task_params)
+  end
+
   def index
+    @customs = Custom.all
   end
 
   def show
@@ -7,4 +16,11 @@ class CustomsController < ApplicationController
 
   def edit
   end
+
+  private
+
+  def task_params
+    params.require(:task).permit(:title, :displayed, :use_comment, :use_picture, :mentor)
+  end
+
 end
--- app/models/custom.rb
+++ app/models/custom.rb
@@ -1,3 +1,8 @@
 class Custom < ApplicationRecord
   belongs_to :user
+  
+  # タスクとの連携
+  has_many :tasks, dependent: :destroy
+  accepts_nested_attributes_for :tasks, reject_if: :all_blank, allow_destroy: true
+  
 end
