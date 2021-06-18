55f9dff14ebffbc6ef18f3e6146ea3a3d9e658a7
hiroyuki_yamashita <hirokore3221@gmail.com>
2021-06-18 14:41:29 +0900

    update: kaminariGem追加（ページネーション）及びfooter追加

--- Gemfile
+++ Gemfile
@@ -29,6 +29,8 @@ gem 'mini_magick'
 
 gem 'ransack'
 
+gem 'kaminari'
+
 group :development, :test do
   gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,7 +12,7 @@ class UsersController < ApplicationController
   end
   
   def index
-    @users = User.all
+    @users = User.all.page(params[:page]).per(5)
   end
 
   def edit
